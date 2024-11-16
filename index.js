37b66d0bdf7ce70e5656132cae2fde63785814b5
Bhavya Gor <bhavya.gor9999@gmail.com>
2024-11-17 03:14:03 +0700

    make it work

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import fs from "fs";
 const git = simpleGit();
 
 async function run() {
-  console.log(chalk.blue("Welcome to Create Essential! 🚀"));
+  console.log(chalk.blue("Welcome to Scaffold Essential! 🚀"));
 
   const answers = await inquirer.prompt([
     {
@@ -24,7 +24,7 @@ async function run() {
   console.log(chalk.green("Cloning the repository..."));
   await git.clone(
     "https://github.com/ScaffoldEssential/scaffold-essential.git",
-    projectName
+    projectName,
   );
 
   console.log(chalk.green("Installing frontend dependencies..."));
@@ -35,10 +35,6 @@ async function run() {
 
   console.log(chalk.green("Installing backend dependencies..."));
   execSync("rustup update", { stdio: "inherit" });
-  execSync("cargo install essential-builder", {
-    cwd: `${projectName}/backend`,
-    stdio: "inherit",
-  });
 
   console.log(chalk.green("Setting up project scripts..."));
   const pmConfig = {
@@ -50,8 +46,10 @@ async function run() {
       dev: "cd frontend && npm run dev",
       build: "cd frontend && npm run build",
       compile: "cd backend && pint build",
-      chain: "cd backend && RUST_LOG=trace essential-builder --node-api-bind-address '0.0.0.0:3553' --builder-api-bind-address '0.0.0.0:3554'",
-      shell: "cd backend && nix shell github:essential-contributions/essential-integration#essential",
+      chain:
+        "cd backend && RUST_LOG=trace essential-builder --node-api-bind-address '0.0.0.0:3553' --builder-api-bind-address '0.0.0.0:3554'",
+      shell:
+        "cd backend && nix shell github:essential-contributions/essential-integration#essential",
       generate: "cd frontend && npm run scripts",
     },
     author: "Bhavya Gor & Kenil Shah",
@@ -60,15 +58,16 @@ async function run() {
 
   fs.writeFileSync(
     `${projectName}/package.json`,
-    JSON.stringify(pmConfig, null, 2)
+    JSON.stringify(pmConfig, null, 2),
   );
 
   console.log(chalk.green("All done! 🎉"));
   console.log(chalk.blue(`Your project is ready at ${projectName}.`));
   console.log(chalk.green("To get started:"));
   console.log(chalk.yellow(`  cd ${projectName}`));
-  console.log(chalk.yellow(`  npm run dev`));
+  console.log(chalk.yellow("  npm run dev (to start the frontend)"));
+  console.log(chalk.yellow("  npm run compile (to compile the backend)"));
+  console.log(chalk.yellow("  npm run chain (to start the chain)"));
 }
 
 run().catch((err) => console.error(chalk.red(err)));
-
